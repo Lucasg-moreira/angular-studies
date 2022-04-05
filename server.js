bc48afc9b9f0fde360796336adb690e7a9857e55
Lucasg-moreira <lucasmoreira.gabriel2004@gmail.com>
2022-04-05 10:36:57 -0300

    heroku update

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-import { Express } from "express";
+const express = require("express")
+
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+app.use(express.static(__dirname + "/dist/iniciando-angular"))
 
 app.get("/", (request, response) => {
-  response.send("hello world")
+  response.sendFile(__dirname + "/dist/iniciando-angular/index.html")
 })
-
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("listen on port " + PORT)
